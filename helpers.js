f8e963fb3e8ad9852dc7eb0ab8f3f35070bee5e9
Karma Tenpa <ktenpa@MacBook-Air.local>
2025-01-26 10:18:18 -0600

    feat: implement utility functions in helpers.js and add /mode route to calculate mode

---
+++ helpers.js
@@ -0,0 +1,45 @@
+function createFrequencyCounter(arr) {
+    return arr.reduce(function (acc, next) {
+        acc[next] = (acc[next] || 0) + 1;
+        return acc;
+    }, {});
+}
+
+function findMode(arr) {
+    let freqCounter = createFrequencyCounter(arr);
+
+    let count = 0;
+    let mostFrequent;
+
+    for (let key in freqCounter) {
+        if (freqCounter[key] > count) {
+            mostFrequent = key;
+            count = freqCounter[key];
+        }
+    }
+
+    return +mostFrequent;
+}
+
+// Convert and validate numbers from query
+function convertAndValidateNumsArray(numsAsStrings) {
+    let result = [];
+  
+    for (let i = 0; i < numsAsStrings.length; i++) {
+      let valToNumber = Number(numsAsStrings[i]);
+  
+      if (Number.isNaN(valToNumber)) {
+        return new Error(`The value '${numsAsStrings[i]}' at index ${i} is not a valid number.`);
+      }
+  
+      result.push(valToNumber);
+    }
+    
+    return result;
+  }
+
+module.exports = {
+    createFrequencyCounter,
+    findMode,
+    convertAndValidateNumsArray
+};
