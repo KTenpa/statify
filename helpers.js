6239a51516bb7c71c84fd1f73dd8f23cf4c2ca46
Karma Tenpa <ktenpa@MacBook-Air.local>
2025-01-26 10:25:21 -0600

    feat: add endpoints for mean and median calculations with helper functions
    - Updated app.js:
      - Added /mean and /median routes for calculating mean and median.
      - Enhanced error handling for invalid query inputs.
    
    - Updated helpers.js:
      - Implemented findMean and findMedian helper functions.
      - Improved query validation with convertAndValidateNumsArray.

--- helpers.js
+++ helpers.js
@@ -38,8 +38,32 @@ function convertAndValidateNumsArray(numsAsStrings) {
     return result;
   }
 
+function findMean(nums) {
+    if (nums.length === 0) return 0;
+    return nums.reduce(function (acc, cur) {
+      return acc + cur;
+    }) / nums.length;
+  }
+  
+function findMedian(nums) {
+    nums.sort((a, b) => a - b);
+  
+    let middleIndex = Math.floor(nums.length / 2);
+    let median;
+  
+    if (nums.length % 2 === 0) {
+      median = (nums[middleIndex] + nums[middleIndex - 1]) / 2;
+    } else {
+      median = nums[middleIndex];
+    }
+    
+    return median;
+  }  
+
 module.exports = {
     createFrequencyCounter,
+    findMean,
+    findMedian,
     findMode,
     convertAndValidateNumsArray
 };
