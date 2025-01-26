5b7af9ce6c0e5e7e77474c34c07b7f1f7c7674f5
Karma Tenpa <ktenpa@MacBook-Air.local>
2025-01-26 10:00:33 -0600

    feat: create basic Express app setup

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+// Export the app and the server
+const server = app.listen(3000, function() {
+  console.log(`Server starting on port 3000`);
+});
+
+module.exports = { app, server };
